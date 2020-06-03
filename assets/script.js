8f81b042f90ecfb8bea114fd7d56554a060dc50a
Collin <collinhodgs@gmail.com>
2020-06-03 13:45:03 -0600

    Set constant variables

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+const lowerCase = ['a', 'b', 'c', 'e', 'd', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+const upperCase = ['A', 'B', 'C', 'E', 'D', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+const special = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')'];
+const number = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 
 // Write password to the #password input
 function writePassword() {
