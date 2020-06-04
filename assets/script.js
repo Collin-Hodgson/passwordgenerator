a66096b075f583beac54171cf70359df148f0999
Collin <collinhodgs@gmail.com>
2020-06-04 16:34:16 -0600

    Wrapped "if" statements in a "while" statement

--- assets/script.js
+++ assets/script.js
@@ -6,21 +6,18 @@ const special = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')'];
 const number = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
 
-}
+function generatePassword() {
 
-generateBtn.onclick = function generatePassword(params) {
+  var randomPassword = "";
+
   var passwordLength = prompt("How long would you like your password to be? (Must be between 8-128 characters)");
     while (passwordLength <8 || passwordLength >128) {
       alert("Password length must be between 8-128 characters!");
       var passwordLength = prompt("How long would you like your password to be? (Must be between 8-128 characters)");
     } 
+
   var confirmLower = confirm("Would you like to include lower case letters in your password?");
   var confirmUpper = confirm("Would you like to include upper case letters in your password?");
   var confirmSpecial = confirm("Would you like to include special characters in your password?");
@@ -33,38 +30,46 @@ generateBtn.onclick = function generatePassword(params) {
       var confirmNumber = confirm("Would you like to include number in your password?");
       
     }
-  if (confirmLower === true) {
-    for (let i = 0; i < lowerCase.length; i++) {
-      var randomLower = lowerCase [Math.floor(Math.random() * lowerCase.length)];
-      randomPassword += randomLower;
-      
-    }    
-  }
 
-  if (confirmUpper === true) {
-    for (let i = 0; i < upperCase.length; i++) {
-      var randomUpper = upperCase [Math.floor(Math.random() * upperCase.length)];
-      randomPassword += randomUpper
-      
-    }    
-  }
+while (randomPassword.length < passwordLength) {
 
-  if (confirmSpecial === true) {
-    for (let i = 0; i < special.length; i++) {
-      var randomSpecial = special [Math.floor(Math.random() * special.length)];
-      randomPassword += randomSpecial
-      
-    }    
-  }
+        if (confirmLower === true && randomPassword.length < passwordLength) {
+            var randomLower = lowerCase [Math.floor(Math.random() * lowerCase.length)];
+            randomPassword += randomLower;       
+        }
 
-  if (confirmNumber === true) {
-    for (let i = 0; i < number.length; i++) {
-      var randomNumber = number [Math.floor(Math.random() * number.length)];
-      randomPassword += randomNumber
-      
-    }    
-  }
-  
+        if (confirmUpper === true && randomPassword.length < passwordLength) {
+            var randomUpper = upperCase [Math.floor(Math.random() * upperCase.length)];
+            randomPassword += randomUpper;    
+        }
+
+        if (confirmSpecial === true && randomPassword.length < passwordLength) {
+            var randomSpecial = special [Math.floor(Math.random() * special.length)];
+            randomPassword += randomSpecial   
+        }
+
+        if (confirmNumber === true && randomPassword.length < passwordLength) {
+            var randomNumber = number [Math.floor(Math.random() * number.length)];
+            randomPassword += randomNumber   
+        }
+        if (randomPassword.length < passwordLength) {
+          continue;
+        }
+        else {
+          console.log(randomPassword);
+          return randomPassword;
+        }
+        
+     }
+}
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
 }
 
 // Add event listener to generate button
